d9db757f23d00c15616d83206c0117248b4dc068
7azem1E <hazem.a.abduallh@gmail.com>
2023-03-21 16:57:55 -0700

    test

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,11 @@
  */
 int print_last_digit(int n)
 {
+	int k;
+
+	k = n % 10;
 	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+		k = -k;
+	_putchar(k + '0');
+	return (k);
 }
