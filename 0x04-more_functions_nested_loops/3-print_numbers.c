bf4640af408ae9fd167f09a33ea2ece1819d4169
7azem1E <hazem.a.abduallh@gmail.com>
2023-03-23 18:25:27 -0700

    test

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prinr 0 to 9
+ *
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for ( i = 0 ; i <= 9 ; i++ )
+		_putchar(i);
+	_putchar('\n');
+}
