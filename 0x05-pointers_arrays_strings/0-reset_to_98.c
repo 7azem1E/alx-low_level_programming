c44d4b3420cd10fb1d438c1cf98aa5c219d70d2e
7azem1E <hazem.a.abduallh@gmail.com>
2023-04-17 19:42:35 -0700

    hate c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 : take the pointers and update its value to 98
+ *
+ * @n : parametar
+ *
+ * Return : nothing
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
